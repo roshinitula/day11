7d27cbfeff8793b63e2e8d06408d63c61c144df1
roshinitula <tularoshini@gmail.com>
2025-01-08 10:31:00 +0530

    pushing files to git

---
+++ looping.js
@@ -0,0 +1,40 @@
+//for(let i=0 ; i<=5;i++){
+    //console.log("Good morning")
+//}
+
+// for(let i=1;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i+"is a even number")
+        
+//     }else{
+//         console.log(i+"is a odd number")
+//     }
+// }
+//while loop
+// let i=1;
+// while(i <= 5){
+//     console.log("Good morning");
+//     i++;
+// }
+
+//do
+// let i =1;
+// let n=5;
+// do{
+//     console.log("Hello");
+//     i++;
+// }
+//     while(i>=n)
+
+//break
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+//     if(i==6)break;
+// }
+
+//continue
+// for(let i=1;i<=10;i++){
+//     if(i==6)continue;
+//        console.log(i);
+//     }
+
