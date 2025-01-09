d0ed423f6187f8b30bfb51d4ec4ed8f74f4e5f79
roshinitula <tularoshini@gmail.com>
2025-01-09 11:53:41 +0530

    pushing files to git

---
+++ functions.js
@@ -0,0 +1,43 @@
+// function addToCart(){
+//     alert("product is added to cart");
+// }
+//!function without parameters
+
+// function demo(){
+// console.log("function without parameters");
+// }
+// demo();
+
+//!function with parameters
+
+// function userDetails(name , age, city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+// userDetails("roshini" , 20 , "kamareddy");
+ 
+//!function expression
+// let x = function(){
+//     console.log("function expression is execution")
+// };
+// x();
+//! IIFE(imediate invoke function)
+// (function(){
+//     console.log("IIFE");
+// })();
+
+//! arrow function
+// function demo(){
+//     console.log("Hello");
+// } 
+// demo();
+
+// let x = ()=>{console.log("arrow function");};
+//     x();
+
+let x=_=>{console.log("hi");
+    console.log("hello");
+    console.log("bye");
+};
+x();
