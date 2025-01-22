8aeef5c82552b117ee6de63f4e41f269d461dbd5
roshinitula <tularoshini@gmail.com>
2025-01-22 13:06:07 +0530

    pushing files

--- async.js
+++ async.js
@@ -7,11 +7,25 @@
 // console.log(50);
 
 
-console.log(10);
-console.log(20);
-setInterval(()=>{
-    document.writeln("MRU");
-    console.log(30);
-},150)
-console.log(40);
-console.log(50);
+// console.log(10);
+// console.log(20);
+// setInterval(()=>{
+//     document.writeln("MRU");
+//     console.log(30);
+// },150)
+// console.log(40);
+// console.log(50);
+
+//! API fetching
+
+function fetchUsers(){
+    let response =fetch("https://jsonplaceholder.typicode.com/users");
+    response.then(result=>{
+        // console.log(result.json());
+        return result.json().then(data=>{
+            console.log(data);
+        })
+    })
+    .catch(err=>console.log(err))
+}
+fetchUsers();
