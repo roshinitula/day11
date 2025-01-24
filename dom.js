2b5ae2b63a06c1fb3c4fd99f544c2834d5281371
roshinitula <tularoshini@gmail.com>
2025-01-24 12:55:43 +0530

    pushing files

--- dom.js
+++ dom.js
@@ -1,3 +1,28 @@
-let  element = document.getElementById("demo");
-element.innerText ="G+";
-console.log(element);
+// let  element = document.getElementById("demo");
+// element.innerText ="G+";
+// console.log(element);
+
+//!
+
+// let ele = document.getElementsByClassName("test");
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// ele[0].innerText="Hello"
+// ele[0].style.backgroundColor="tomato"
+
+// //! spread operator:- itwill takeout each and every value from the original array
+// //! and store it inside one more new array(pure array).
+
+// let x=[...ele];
+// x.map(element=>{
+//     element.style.backgroundColor="pink"
+// })
+
+//  let ele = document.querySelector("#mru");
+//  console.log(ele);
+
+//  let ele = document.querySelector(".test");
+//  console.log(ele);
+
+let ele = document.querySelectorAll(".test");
+console.log(ele);
