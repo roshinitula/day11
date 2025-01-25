518e403536806f0a7c2966fa69c8ae50571e4ecc
roshinitula <tularoshini@gmail.com>
2025-01-25 13:01:06 +0530

    pushing files

--- dom.js
+++ dom.js
@@ -24,5 +24,46 @@
 //  let ele = document.querySelector(".test");
 //  console.log(ele);
 
-let ele = document.querySelectorAll(".test");
-console.log(ele);
+// let ele = document.querySelectorAll(".test");
+// console.log(ele);
+
+
+
+
+
+
+// let ele=document.createElement("h1");
+// ele.innerText="Dynamic creation of html element";
+// //ele.setAttribute("id","demo");
+// ele.id="demo";
+// //ele.removeAttribute("id")
+// //console.log(ele);
+// document.body.append(ele);
+
+// let image=document.createElement("img");
+// image.src="./3606208.jpg";
+// console.log(image);
+
+// document.body.appendChild(ele);
+// document.body.appendChild(image)
+
+
+
+let form = document.querySelector("form");
+let username =document.getElementById("uName");
+let mail = document.getElementById("uMail");
+let psw = document.getElementById("uPass");
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let uname = username.ariaValueMax;
+    let uemail = mail.ariaValueMax;
+    let upass = psw.ariaValueMax;
+    let userDetails={
+        uname, uemail, upass
+    }
+
+    
+    console.log(userDetails);
+    localStorage.setItem("userData", JSON.stringify(userDetails))
+})
