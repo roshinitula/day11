8f610ff9529734a539e1ea8afb8becc4c3f117d3
roshinitula <tularoshini@gmail.com>
2025-01-27 14:59:03 +0530

    pushing files

--- dom.js
+++ dom.js
@@ -49,21 +49,127 @@
 
 
 
-let form = document.querySelector("form");
-let username =document.getElementById("uName");
-let mail = document.getElementById("uMail");
-let psw = document.getElementById("uPass");
+// let form = document.querySelector("form");
+// let username =document.getElementById("uName");
+// let mail = document.getElementById("uMail");
+// let psw = document.getElementById("uPass");
 
-form.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    let uname = username.ariaValueMax;
-    let uemail = mail.ariaValueMax;
-    let upass = psw.ariaValueMax;
-    let userDetails={
-        uname, uemail, upass
-    }
+// form.addEventListener("submit",(event)=>{
+//     event.preventDefault();
+//     let uname = username.ariaValueMax;
+//     let uemail = mail.ariaValueMax;
+//     let upass = psw.ariaValueMax;
+//     let userDetails={
+//         uname, uemail, upass
+//     }
 
     
-    console.log(userDetails);
-    localStorage.setItem("userData", JSON.stringify(userDetails))
-})
+//     console.log(userDetails);
+//     localStorage.setItem("userData", JSON.stringify(userDetails))
+// })
+
+
+// let main=document.createElement("div");
+// main.setAttribute("id","mainblock");
+// console.log(main);
+
+// let topEle=document.createElement("div");
+// topEle.setAttribute("class","topblock");
+
+// let image=document.createElement("img");
+// image.setAttribute("src","/paris1.png")
+// topEle.appendChild(image);
+// // console.log(topEle);
+
+// let bottomEle=document.createElement("div");
+// bottomEle.setAttribute("class","bottomblock");
+// // console.log(bottomEle);
+
+// let h1=document.createElement("h1");
+// h1.innerText("Paris");
+
+// let btn=document.createElement("button");
+// btn.innerText("View more");
+
+// bottomEle.appendChild(h1);
+// bottomEle.appendChild(btn);
+
+// main.appendChild(topEle);
+// main.appendChild(bottomEle);
+
+// document.body.appendChild(main);
+
+
+// let main=document.createElement("div");
+// main.setAttribute("id","mainblock");
+// console.log(main);
+
+// let topEle=document.createElement("div");
+// topEle.setAttribute("class","topblock");
+
+// let image=document.createElement("img");
+// image.src ="./3606208.jpeg"
+// topEle.appendChild(image);
+// // console.log(topEle);
+
+// let bottomEle=document.createElement("div");
+// bottomEle.setAttribute("class","bottomblock");
+// // console.log(bottomEle);
+
+// let h1=document.createElement("h1");
+// h1.innerText="car";
+
+// let btn=document.createElement("button");
+// btn.innerText="View more";
+// btn.style.border="none";
+// btn.style.padding="20px";
+// btn.style.color="white";
+// btn.style.backgroundColor="doggerblue";
+
+// bottomEle.appendChild(h1);
+// bottomEle.appendChild(btn);
+
+// main.appendChild(topEle);
+// main.appendChild(bottomEle);
+
+// document.body.appendChild(main);
+
+
+
+let mainEle = document.createElement("div");
+mainEle.setAttribute("id","mainBlock");
+mainEle.style.border="2px solid red";
+mainEle.style.width="550px";
+mainEle.style.height="550px";
+console.log(mainEle);
+
+let topEle = document.createElement("div");
+topEle.setAttribute("class","topBlock");
+
+let image = document.createElement("img");
+image.src="https://cdn.pixabay.com/photo/2023/02/09/16/43/cat-7779237_640.jpg";
+image.style.width="550px"
+
+let bottomEle = document.createElement("div");
+bottomEle.setAttribute("class","bottomBlock");
+
+let h1 = document.createElement("h1");
+h1.innerText = "CAT";
+h1.style.textAlign="center"
+
+let btn = document.createElement("button");
+btn.innerText="View More";
+btn.style.border="none";
+btn.style.padding="20px";
+btn.style.backgroundColor="dodgerblue";
+btn.style.color="white";
+
+bottomEle.appendChild(h1);
+bottomEle.appendChild(btn);
+
+topEle.appendChild(image);
+mainEle.appendChild(topEle);
+mainEle.appendChild(bottomEle);
+document.body.appendChild(mainEle);
+
+
